6d0422f5ce5240bd4e6cc96072e60da4b09b9475
Amory Galili <amorygalili@gmail.com>
2021-07-03 14:44:06 -0400

    Added tests

--- src/element-config/index.test.js
+++ src/element-config/index.test.js
@@ -1,7 +1,44 @@
 import { normalizeConfig } from './index';
 
+const getConfig = ({
+  name = 'some-element-name',
+  description = '',
+  properties = [],
+  events = [],
+  slots = [],
+  cssProperties = [],
+  cssParts = [],
+} = {}) => ({
+  name,
+  description,
+  properties,
+  events,
+  slots,
+  cssProperties,
+  cssParts,
+});
+
 describe('Element config', () => {
-  it('does stuff', () => {
-    expect(2).toBe(2);
+  it(`returns the same configuration if it's already valid`, () => {
+    const config = getConfig();
+    expect(config).toEqual(normalizeConfig(config));
+  });
+
+  it(`adds default values`, () => {
+    expect(
+      normalizeConfig({ name: 'some-element-name' })
+    ).toEqual(getConfig());
+  });
+
+  it(`throws an error if the element name isn't in kebab case`, () => {
+    expect(() => {
+      normalizeConfig({ name: 'invalidName' })
+    }).toThrow();
+  });
+
+  it(`throws an error if the element name isn't provided`, () => {
+    expect(() => {
+      normalizeConfig({})
+    }).toThrow();
   });
 });
--- src/element-config/pattern-testers.js
+++ src/element-config/pattern-testers.js
@@ -2,21 +2,21 @@ import { camelCase, kebabCase, typePattern } from "./patterns";
 import { camelCaseError, kebabCaseError, typeError } from './error-messages';
 
 export const normalizeCamelCase = (name, errorType) => {
-  if (!camelCase.test(name)) {
+  if (typeof name !== 'string' || !camelCase.test(name)) {
     throw new Error(camelCaseError(errorType));
   }
   return name;
 };
 
 export const normalizeKebabCase = (name, errorType) => {
-  if (!kebabCase.test(name)) {
+  if (typeof name !== 'string' || !kebabCase.test(name)) {
     throw new Error(kebabCaseError(errorType));
   }
   return name;
 };
 
 export const normalizeType = (type, errorType) => {
-  if (!typePattern.test(type)) {
+  if (typeof name !== 'string' || !typePattern.test(type)) {
     throw new Error(typeError(errorType));
   }
   return type;
