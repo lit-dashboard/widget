19fa18ac3ba0ffdf9b1d76cc0a8fdffa0c44ae06
Amory Galili <amorygalili@gmail.com>
2021-11-04 01:16:52 -0400

    Added typescript config. Added functions to filter elements that are webbits

---
+++ packages/webbit/src/filter.ts
@@ -0,0 +1,90 @@
+
+type StringOrFalse = false | string;
+
+type ElementConfig = {
+  defaultSourceKey: StringOrFalse,
+  defaultSourceProvider: StringOrFalse,
+};
+
+function isStringOrFalse(value: any): value is StringOrFalse {
+  return value === false || typeof value === 'string';
+}
+
+function isElementConfig(config: object): config is ElementConfig {
+  const { defaultSourceKey, defaultSourceProvider } = (config as ElementConfig);
+  return isStringOrFalse(defaultSourceKey) && isStringOrFalse(defaultSourceProvider);
+}
+
+function getMatchingElementConfig(
+  elementConfigs: Map<string, object>, 
+  element: HTMLElement
+): object | undefined {
+  const entry = [...elementConfigs.entries()]
+    .find(([selector]) => element.matches(selector));
+  return entry?.[1];
+}
+
+function createNodeFilter(
+  defaultSourceProvider: StringOrFalse, 
+  elementConfigs: Map<string, object>
+) : (node: Node) => number {
+
+
+  return function(node: Node) : number {
+
+    const element = node as HTMLElement;
+
+    const elementConfig = getMatchingElementConfig(elementConfigs, element);
+
+    // Skip if there's no valid element config
+    if (
+      typeof elementConfig === 'undefined'
+      || !isElementConfig(elementConfig)
+    ) {
+      return NodeFilter.FILTER_SKIP;
+    }
+
+    // Skip if the element does not have a source provider
+    if (
+      !element.hasAttribute('source-provider')
+      && typeof elementConfig.defaultSourceProvider === 'undefined'
+      && typeof defaultSourceProvider === 'undefined'
+    ) {
+      return NodeFilter.FILTER_SKIP;
+    }
+
+    // Skip if the element does not have a source key
+    if (
+      !element.hasAttribute('source-key')
+      && typeof elementConfig.defaultSourceKey === 'undefined'
+    ) {
+      return NodeFilter.FILTER_SKIP;
+    }
+
+    return NodeFilter.FILTER_ACCEPT;
+  }
+}
+
+function getWebbitIterator(
+  root: HTMLElement,
+  defaultSourceProvider: StringOrFalse, 
+  elementConfigs: Map<string, object>
+) {
+  return document.createNodeIterator(
+    root, 
+    NodeFilter.SHOW_ELEMENT, 
+    createNodeFilter(defaultSourceProvider, elementConfigs)
+  );
+}
+
+function getWebbitTreeWalker(
+  root: HTMLElement,
+  defaultSourceProvider: StringOrFalse, 
+  elementConfigs: Map<string, object>
+)  {
+  return document.createTreeWalker(
+    root,
+    NodeFilter.SHOW_ELEMENT,
+    createNodeFilter(defaultSourceProvider, elementConfigs),
+  );
+}
