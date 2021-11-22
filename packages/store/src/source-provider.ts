fa987de821dd7c5f17b84674855c0710bd21d4ed
Amory Galili <amorygalili@gmail.com>
2021-11-21 23:26:32 -0500

    Refactored SourceProvider class

--- packages/store/src/source-provider.ts
+++ packages/store/src/source-provider.ts
@@ -1,5 +1,5 @@
-/* eslint no-underscore-dangle: off */
-import Store from './store';
+/* eslint @typescript-eslint/no-empty-function: off */
+const noop: () => unknown = () => {};
 
 type SourceUpdate = {
   type: string,
@@ -13,66 +13,15 @@ type SourceUpdates = {
   }
 }
 
-type ProviderConstructor = {
-  typeName?: string,
-  settingsDefaults: Record<string, unknown>
-};
+class SourceProvider {
+  #sourceUpdates: SourceUpdates = {};
+  #clearSourcesTimeoutId?: NodeJS.Timeout;
+  #clearSourcesListeners: Array<() => void> = [];
+  #sourcesChangedListeners: Array<(changes: Record<string, unknown>) => void> = [];
+  #sourcesRemovedListeners: Array<(removals: Array<string>) => void> = [];
 
-abstract class SourceProvider {
-  static get __WEBBIT_CLASSNAME__(): string {
-    return 'SourceProvider';
-  }
-
-  static get typeName(): string | undefined {
-    return undefined;
-  }
-
-  static get settingsDefaults(): Record<string, unknown> {
-    return {};
-  }
-
-  private store: Store;
-  private _providerName: string;
-  public settings: Record<string, unknown>;
-  private _sourceUpdates: SourceUpdates;
-  private _interval: ReturnType<typeof setInterval>;
-  private _clearSourcesTimeoutId?: NodeJS.Timeout
-
-  /**
-   * Parent class all source providers must inherit from. Each source provider
-   * instance is responsible for maintaining its own state object in the store.
-   *
-   * @memberof module:@webbitjs/store
-   * @abstract
-   * @param {string} providerName - The name of the provider.
-   */
-  constructor(store: Store, providerName: string, settings: Record<string, unknown>) {
-    if (new.target === SourceProvider) {
-      throw new TypeError('Cannot construct SourceProvider instances directly.');
-    }
-
-    if (typeof providerName !== 'string') {
-      throw new TypeError("The providerName needs to be passed into super() from your provider's constructor.");
-    }
-
-    if (typeof settings === 'undefined') {
-      throw new Error("settings must be passed into the super() from your provider's constructor.");
-    }
-
-    const constructor = this.constructor as unknown as ProviderConstructor;
-
-    if (typeof constructor.typeName !== 'string') {
-      throw new Error('A typeName string must be defined.');
-    }
-
-    this.store = store;
-    this._providerName = providerName;
-    this.settings = {
-      ...constructor.settingsDefaults,
-      ...settings,
-    };
-    this._sourceUpdates = {};
-    this._interval = setInterval(this._sendUpdates.bind(this), 100);
+  constructor() {
+    setInterval(this.#sendUpdates.bind(this), 100);
   }
 
   /**
@@ -86,17 +35,17 @@ abstract class SourceProvider {
    * @param {*} value - The new value.
    */
   updateSource(key: string, value: unknown): void {
-    clearTimeout(this._clearSourcesTimeoutId);
+    clearTimeout(this.#clearSourcesTimeoutId);
 
-    if (this._sourceUpdates[key] === undefined) {
-      this._sourceUpdates[key] = {
+    if (this.#sourceUpdates[key] === undefined) {
+      this.#sourceUpdates[key] = {
         first: {
           type: 'change',
           value,
         },
       };
     } else {
-      this._sourceUpdates[key].last = {
+      this.#sourceUpdates[key].last = {
         type: 'change',
         value,
       };
@@ -113,72 +62,17 @@ abstract class SourceProvider {
    * by '/'.
    */
   removeSource(key: string): void {
-    if (this._sourceUpdates[key] === undefined) {
-      this._sourceUpdates[key] = {
+    if (this.#sourceUpdates[key] === undefined) {
+      this.#sourceUpdates[key] = {
         first: {
           type: 'removal',
         },
       };
     } else {
-      this._sourceUpdates[key].last = {
+      this.#sourceUpdates[key].last = {
         type: 'removal',
       };
     }
-  }
-
-  /**
-   * Subscribes to changes for a particular source and all that source's
-   * children.
-   *
-   * @param {string} key - The source's key. This is a string separated
-   * by '/'.
-   * @param {function} callback - A function that takes in the source's
-   * value, key, and key of child source that changed.
-   * @param {boolean} callImmediately - If true, the callback is called
-   * immediately with the source's current value.
-   */
-  subscribe(
-    key: string,
-    callback: (value: unknown, sourceKey: string, childKey: string) => unknown,
-    callImmediately: boolean,
-  ): unknown {
-    return this.store.subscribe(this._providerName, key, callback, callImmediately);
-  }
-
-  /**
-   * Subscribes to all source changes.
-   *
-   * @param {function} callback - A function that takes in the source's
-   * value, key, and key of child source that changed.
-   * @param {boolean} callImmediately - If true, the callback is called
-   * immediately with the source's current value.
-   */
-  subscribeAll(
-    callback: (value: unknown, sourceKey: string, childKey: string) => unknown,
-    callImmediately: boolean,
-  ): unknown {
-    return this.store.subscribeAll(this._providerName, callback, callImmediately);
-  }
-
-  /**
-   * Gets a source's value.
-   *
-   * @param {string} key - The source's key. This is a string separated
-   * by '/'.
-   */
-  getSource(key: string): unknown {
-    return this.store.getSource(this._providerName, key);
-  }
-
-  getRawSource(key: string): unknown {
-    return this.store.getRawSource(this._providerName, key);
-  }
-
-  /**
-   * Gets all sources
-   */
-  getSources(): unknown {
-    return this.store.getSources(this._providerName);
   }
 
   /**
@@ -189,14 +83,12 @@ abstract class SourceProvider {
    * @param {function} callback - An optional callback. Called when sources
    * have been cleared.
    */
-  clearSources(callback: () => void): void {
+  clearSources(callback: () => void = noop): void {
     // send updates now to prevent them from being incorrectly sent after
     // sources were cleared.
-    this._sendUpdates(() => {
-      this.store.clearSources(this._providerName);
-      if (typeof callback === 'function') {
-        callback();
-      }
+    this.#sendUpdates(() => {
+      this.#clearSourcesListeners.forEach(listener => listener());
+      callback();
     });
   }
 
@@ -214,8 +106,8 @@ abstract class SourceProvider {
    * have been cleared.
    */
   clearSourcesWithTimeout(timeout: number, callback: () => void): void {
-    clearTimeout(this._clearSourcesTimeoutId);
-    this._clearSourcesTimeoutId = setTimeout(() => {
+    clearTimeout(this.#clearSourcesTimeoutId);
+    this.#clearSourcesTimeoutId = setTimeout(() => {
       this.clearSources(callback);
     }, timeout);
   }
@@ -230,7 +122,9 @@ abstract class SourceProvider {
    * by '/'.
    * @param {*} value - The source's updated value.
    */
-  abstract userUpdate(key: string, value: unknown): void;
+  userUpdate(key: string, value: unknown): void {
+    this.updateSource(key, value);
+  }
 
   /**
    * Helper function to get the type of a variable represented
@@ -254,68 +148,70 @@ abstract class SourceProvider {
     return null;
   }
 
-  _disconnect(): void {
-    clearTimeout(this._interval);
-  }
-
-  _sendUpdates(callback: () => unknown): void {
-    if (Object.keys(this._sourceUpdates).length === 0) {
-      if (typeof callback === 'function') {
-        callback();
-      }
+  #sendUpdates(callback: () => unknown = noop): void {
+    if (Object.keys(this.#sourceUpdates).length === 0) {
+      callback();
       return;
     }
     // send first updates then last
     const firstUpdates = {};
     const lastUpdates = {};
 
-    Object.entries(this._sourceUpdates).forEach(([key, values]) => {
+    Object.entries(this.#sourceUpdates).forEach(([key, values]) => {
       firstUpdates[key] = values.first;
       if ('last' in values) { lastUpdates[key] = values.last; }
     });
 
-    this._sendChanges(firstUpdates);
-    this._sendRemovals(firstUpdates);
+    this.#sendChanges(firstUpdates);
+    this.#sendRemovals(firstUpdates);
 
     if (Object.keys(lastUpdates).length > 0) {
       setTimeout(() => {
-        this._sendChanges(lastUpdates);
-        this._sendRemovals(lastUpdates);
-        this._sourceUpdates = {};
-        if (typeof callback === 'function') {
-          callback();
-        }
+        this.#sendChanges(lastUpdates);
+        this.#sendRemovals(lastUpdates);
+        this.#sourceUpdates = {};
+        callback();
       });
     } else {
-      this._sourceUpdates = {};
-      if (typeof callback === 'function') {
-        callback();
-      }
+      this.#sourceUpdates = {};
+      callback();
     }
   }
 
-  _sendChanges(updates: { [key: string]: SourceUpdate }): void {
-    const changes = {};
+  #sendChanges(updates: { [key: string]: SourceUpdate }): void {
+    const changes: Record<string, unknown> = {};
     Object.entries(updates).forEach(([key, { type }]) => {
       if (type === 'change') {
         changes[key] = updates[key].value;
       }
     });
     if (Object.keys(changes).length > 0) {
-      this.store.sourcesChanged(this._providerName, changes);
+      this.#sourcesChangedListeners.forEach(listener => listener(changes));
     }
   }
 
-  _sendRemovals(updates: { [key: string]: SourceUpdate }): void {
-    const removals = [];
+  #sendRemovals(updates: { [key: string]: SourceUpdate }): void {
+    const removals: Array<string> = [];
     Object.entries(updates).forEach(([key, { type }]) => {
       if (type === 'removal') {
         removals.push(key);
       }
     });
     if (removals.length > 0) {
-      this.store.sourcesRemoved(this._providerName, removals);
+      this.#sourcesRemovedListeners.forEach(listener => listener(removals));
     }
+  }
+
+  addClearSourcesListener(listener: () => unknown): void {
+    this.#clearSourcesListeners.push(listener);
+  }
+
+  addSourcesChangedListener(listener: (changes: Record<string, unknown>) => void): void {
+    this.#sourcesChangedListeners.push(listener);
+  }
+
+  addSourcesRemovedListener(listener: (removals: Array<string>) => void): void {
+    this.#sourcesRemovedListeners.push(listener);
   }
 }
 
--- packages/store/src/store/index.ts
+++ packages/store/src/store/index.ts
@@ -1,37 +1,39 @@
 import Sources from './sources';
+import SourceProvider from '../source-provider';
+
+type ProviderClass = {
+  typeName: string,
+  __WEBBIT_CLASSNAME__: 'SourceProvider',
+};
 
 /**
  @module @webbitjs/store
 */
 class Store {
-
-  constructor() {
-    this.providerTypes = {};
-    this.providers = {};
-    this.defaultSourceProvider = null;
-    this.sourceProviderListeners = [];
-    this.defaultSourceProviderListeners = [];
-    this.sources = new Sources(this);
-  }
+  private providerTypes: Record<string, typeof SourceProvider> = {};
+  private providers: Record<string, SourceProvider> = {};
+  private defaultSourceProvider: string | null = null;
+  private sourceProviderListeners: Array<(provider: string) => unknown> = [];
+  private defaultSourceProviderListeners: Array<(provider: string) => unknown> = [];
+  private sources = new Sources();
 
   /**
  * Adds a provider type.
- * 
+ *
  * @function
  * @example
  * import { SourceProvider, addSourceProviderType } from "@webbitjs/store";
- * 
+ *
  * class MyProvider extends SourceProvider {
  *   // class body
  * }
- * 
+ *
  * addSourceProviderType(MyProvider);
- * 
+ *
  * @param {SourceProvider} constructor - The source provider class
  */
-  addSourceProviderType(constructor) {
-
-    const { typeName } = constructor;
+  addSourceProviderType(constructor: unknown): void {
+    const { typeName, __WEBBIT_CLASSNAME__ } = constructor as typeof SourceProvider;
 
     if (typeof typeName !== 'string') {
       throw new Error('A typeName for your source provider type must be set.');
@@ -41,42 +43,39 @@ class Store {
       throw new Error('A source provider type with the same name has already been added.');
     }
 
-    if (constructor.__WEBBIT_CLASSNAME__ === 'SourceProvider') {
-      this.providerTypes[typeName] = constructor;
+    if (__WEBBIT_CLASSNAME__ === 'SourceProvider') {
+      this.providerTypes[typeName] = constructor as typeof SourceProvider;
     }
   }
 
-  hasSourceProviderType(typeName) {
+  hasSourceProviderType(typeName: string): boolean {
     return typeName in this.providerTypes;
   }
 
-  hasSourceProvider (providerName) {
+  hasSourceProvider(providerName: string): boolean {
     return providerName in this.providers;
   }
 
-  addSourceProvider(providerType, providerName, settings) {
-
-    settings = settings || {};
-  
-    if (typeof providerName !== 'string') {
-      providerName = providerType;
-    }
-  
+  addSourceProvider(
+    providerType: string,
+    providerName: string = providerType,
+    settings: Record<string, unknown> = {},
+  ): SourceProvider {
     if (!this.hasSourceProviderType(providerType)) {
-      throw new Error(`A source provider type with that name hasn't been added.`);
+      throw new Error('A source provider type with that name hasn\'t been added.');
     }
-  
+
     if (this.hasSourceProvider(providerName)) {
       throw new Error('A source provider with that name has already been added.');
     }
-  
+
     const SourceProvider = this.providerTypes[providerType];
-  
+
     this.providers[providerName] = new SourceProvider(this, providerName, {
       ...SourceProvider.settingsDefaults,
-      ...settings
+      ...settings,
     });
-  
+
     this.sourceProviderListeners.forEach(listener => {
       listener(providerName);
     });
@@ -87,7 +86,7 @@ class Store {
     if (typeof listener !== 'function') {
       throw new Error('listener is not a function');
     }
-  
+
     this.sourceProviderListeners.push(listener);
   }
 
@@ -95,7 +94,7 @@ class Store {
     if (!this.hasSourceProvider(providerName)) {
       return;
     }
-  
+
     const provider = this.providers[providerName];
     provider._disconnect();
     delete this.providers[providerName];
@@ -104,18 +103,18 @@ class Store {
   getSourceProvider(providerName) {
     return this.providers[providerName];
   }
-  
+
   getSourceProviderTypeNames() {
     return Object.keys(this.providerTypes);
   }
-  
+
   getSourceProviderNames() {
     return Object.keys(this.providers);
-  };
+  }
 
   setDefaultSourceProvider(providerName) {
     this.defaultSourceProvider = providerName;
-  
+
     this.defaultSourceProviderListeners.forEach(listener => {
       listener(this.defaultSourceProvider);
     });
@@ -125,37 +124,37 @@ class Store {
     if (typeof listener !== 'function') {
       throw new Error('listener is not a function');
     }
-  
+
     this.defaultSourceProviderListeners.push(listener);
   }
 
-  getDefaultSourceProvider () {
+  getDefaultSourceProvider() {
     return this.defaultSourceProvider;
   }
 
   getRawSources(providerName) {
     return this.sources.getRawSources(providerName);
-  };
-  
+  }
+
   getRawSource(providerName, key) {
     return this.sources.getRawSource(providerName, key);
   }
-  
+
   getSources(providerName) {
     return this.sources.getSources(providerName);
-  };
-  
+  }
+
   getSource(providerName, key) {
     return this.sources.getSource(providerName, key);
-  };
-  
+  }
+
   subscribe(providerName, key, callback, callImmediately) {
     return this.sources.subscribe(providerName, key, callback, callImmediately);
-  };
-  
+  }
+
   subscribeAll(providerName, callback, callImmediately) {
     return this.sources.subscribeAll(providerName, callback, callImmediately);
-  };
+  }
 
   sourcesChanged(providerName, sourceChanges) {
     return this.sources.sourcesChanged(providerName, sourceChanges);
@@ -164,7 +163,7 @@ class Store {
   clearSources(providerName) {
     return this.sources.clearSources(providerName);
   }
-  
+
   sourcesRemoved(providerName, sourceRemovals) {
     return this.sources.sourcesRemoved(providerName, sourceRemovals);
   }
