d5dde009ac3fcbda9d43abbd78ae3ee88dbe9880
Amory Galili <amorygalili@gmail.com>
2021-11-24 14:55:44 -0500

    Refactored code

--- packages/store/src/store/sources/source-object.ts
+++ packages/store/src/store/sources/source-object.ts
@@ -1,62 +1,67 @@
+/* eslint-disable no-underscore-dangle */
 import { normalizeKey } from '../../util';
-import { Source } from './source-factory';
+import { RawSource, Source } from './source-factory';
+// eslint-disable-next-line import/no-cycle
+import Sources from '.';
 
 class SourceObjects {
-
-  constructor(sources) {
+  readonly sources: Sources;
+  readonly sourceObjectRefs: Record<string, Record<string, Source>> = {};
+  constructor(sources: Sources) {
     this.sources = sources;
-    this.sourceObjectRefs = {};
   }
 
-  getSourceObject(providerName, key) {
-
+  getSourceObject(providerName: string, key: string): Source {
     const normalizedKey = normalizeKey(key);
-  
+
     if (typeof this.sourceObjectRefs[providerName] === 'undefined') {
       this.sourceObjectRefs[providerName] = {};
     }
-  
+
     if (typeof this.sourceObjectRefs[providerName][normalizedKey] === 'undefined') {
       this.sourceObjectRefs[providerName][normalizedKey] = new Source();
     }
-  
+
     return this.sourceObjectRefs[providerName][normalizedKey];
   }
 
-  setSourceObjectProps(providerName, rawSource) {
+  setSourceObjectProps(providerName: string, rawSource: RawSource): void {
     const sourceObject = this.getSourceObject(providerName, rawSource.__normalizedKey__);
-    const sources = this.sources;
-  
+    const { sources } = this;
+
     Object.getOwnPropertyNames(sourceObject).forEach(prop => {
       if (prop in rawSource.__sources__) {
         return;
       }
       delete sourceObject[prop];
     });
-  
-    for (let key in rawSource.__sources__) {
-  
+
+    const rawSourceKeys = Object.keys(rawSource.__sources__);
+
+    for (let i = 0; i < rawSourceKeys.length; i += 1) {
+      const key = rawSourceKeys[i];
       const normalizedKey = normalizeKey(key);
       if (normalizedKey in sourceObject) {
+        // eslint-disable-next-line no-continue
         continue;
       }
-  
+
       const rawSubSource = rawSource.__sources__[key];
       Object.defineProperty(sourceObject, normalizedKey, {
         configurable: true,
         set(value) {
           const providerSources = sources[providerName];
-  
+
           if (typeof providerSources === 'undefined') {
             return;
           }
-  
+
           const setter = providerSources.setters[rawSubSource.__normalizedKey__];
-  
+
           if (typeof setter === 'undefined') {
             return;
           }
-  
+
           setter(value);
         },
         get() {
@@ -64,7 +69,7 @@ class SourceObjects {
             return undefined;
           }
           return sources[providerName].getterValues[rawSubSource.__normalizedKey__];
-        }
+        },
       });
     }
   }
