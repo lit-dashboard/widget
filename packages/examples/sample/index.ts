9d1263ea95e3c7a8e2a4daa7fcc1088a3f248542
Amory Galili <amorygalili@gmail.com>
2021-12-04 22:38:31 -0500

    Restructured examples code

---
+++ packages/examples/sample/index.ts
@@ -0,0 +1,28 @@
+alert('sddsdfds');
+// import Store, { SampleProvider } from '@webbitjs/store';
+// import { WebbitConnector } from '@webbitjs/webbit';
+// import { elementConfigs } from '@frc-web-components/components';
+// import nativeElementConfig from '@webbitjs/native-elements';
+// import '@webbitjs/dashboard';
+
+// const store = new Store();
+// store.addSourceProviderType(SampleProvider);
+// store.addSourceProvider('SampleProvider', 'SampleProvider');
+// store.setDefaultSourceProvider('SampleProvider');
+// const connector = new WebbitConnector(store, {
+//   ...elementConfigs,
+//   ...nativeElementConfig,
+// });
+// window.connector = connector;
+// window.store = store;
+// window.provider = store.getSourceProvider('SampleProvider');
+
+// window.provider.updateSource('/numberBar', 3);
+// window.provider.updateSource('/checkbox/checked', true);
+// window.provider.updateSource('/checkbox/title', "box");
+
+// window.provider.updateSource('/button/value', "eee");
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   connector.connectChildren(document.body);
+// }, false);
--- packages/examples/vite.config.js
+++ packages/examples/vite.config.js
@@ -1,10 +1,20 @@
 const path = require('path');
 
 module.exports = {
+  // build: {
+  //   lib: {
+  //     entry: path.resolve(__dirname, 'src/index.ts'),
+  //     formats: ['es'],
+  //     fileName: () => 'index.js',
+  //     name: 'webbit'
+  //   }
+  // }
   build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/index.js'),
-      name: 'webbit'
+    rollupOptions: {
+      input: {
+        'index': path.resolve(__dirname, 'sample/index.html'),
+        'gamepad': path.resolve(__dirname, 'gamepad/index.html'),
+      }
     }
   }
 }
